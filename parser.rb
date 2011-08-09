19c9811c41f296d61c066f0c5a62f41e7652e3b9
Stefan Rohlfing <stefan.rohlfing@gmail.com>
2011-08-09 16:32:52 +0800

    working on the parser, adding 'textdata'

--- parser.rb
+++ parser.rb
@@ -3,14 +3,15 @@ require 'parslet/convenience'
 require 'pp'
 
 text = File.read('data/codons.txt')
-pp text
+# pp text
 
 class CodonsParser < Parslet::Parser
 
-  rule(:file)            {(line >> newline).repeat(1)}          
-  rule(:line)            {(content.as(:codon) | no_value.as(:comment)).repeat}
-  rule(:content)         {(match('^\s{2}\d') >> any) >> repeat(1)}
-  rule(:no_value)        {(match('^\s{2}\d').absent? >> any).repeat(1)}
+  rule(:file)            {(line >> newline.maybe).repeat}          
+  rule(:line)            {content.as(:value) | no_value.as(:comment)}
+  rule(:content)         {match('\s{2}\d+\s') >> textdata.repeat}
+  rule(:no_value)        {match('\s{2}\d+\s').absent? >> textdata.repeat(1)}
+  rule(:textdata)        {((lf | cr).absent? >> any).repeat(1)}
   # rule(:content)         {long_name >> short_name.maybe >> id >> ncbieaa >> sncbieaa}
   # rule(:long_name)       {}
   # rule(:short_name)      {}
@@ -18,7 +19,7 @@ class CodonsParser < Parslet::Parser
   # rule(:ncbieaa)         {}
   # rule(:sncbieaa)        {}
 
-  rule(:newline)         {lf >> cr.maybe}
+  rule(:newline)         {lf.repeat(1) >> cr.maybe}
   rule(:lf)              {str("\n")}
   rule(:cr)              {str("\r")}
   
@@ -26,4 +27,22 @@ class CodonsParser < Parslet::Parser
 end
 
 pp CodonsParser.new.parse_with_debug(text)
+# EVERY line is parsed as a comment:
+
+# {:comment=>
+#    "--**************************************************************************"@0},
+#  {:comment=>"--  This is the NCBI genetic code table"@77},
+#  {:comment=>
+#    "--  Initial base data set from Andrzej Elzanowski while at PIR International"@117},
+# ...
+# ...
+# {:comment=>"  name \"Thraustochytrium Mitochondrial\" ,"@10199},
+#  {:comment=>"  id 23 ,"@10241},
+#  {:comment=>
+#    "  ncbieaa  \"FF*LSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG\","@10251},
+#  {:comment=>
+#    "  sncbieaa \"--------------------------------M--M---------------M------------\""@10330},
+# ...
+#  {:comment=>"}"@10642}]
+
 
